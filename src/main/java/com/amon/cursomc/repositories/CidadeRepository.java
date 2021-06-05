ab8221fa9f0a84f483b9a55d2eda75e35b505d18
Amon Silva <Amon Silva@italtel.com.br>
2021-06-05 11:09:08 -0300

    Estado e cidade

---
+++ src/main/java/com/amon/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,10 @@
+package com.amon.cursomc.repositories;
+
+import com.amon.cursomc.domain.Cidade;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+}

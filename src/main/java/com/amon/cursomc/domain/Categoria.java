9228768fd7476ea2e5fd243b23044c30d140b947
Amon Silva <Amon Silva@italtel.com.br>
2021-06-05 09:21:53 -0300

    Produto e associações muitos para muitos com categoria

--- src/main/java/com/amon/cursomc/domain/Categoria.java
+++ src/main/java/com/amon/cursomc/domain/Categoria.java
@@ -1,10 +1,9 @@
 package com.amon.cursomc.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -16,6 +15,9 @@ public class Categoria implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY) // tipo de geração de chave primária
     private Integer id;
     private String nome;
+
+    @ManyToMany(mappedBy = "categorias")
+    private List<Produto> produtos = new ArrayList<>();
 
     public Categoria() {
     }
@@ -41,12 +43,20 @@ public class Categoria implements Serializable {
         this.nome = nome;
     }
 
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Categoria)) return false;
-        Categoria that = (Categoria) o;
-        return getId().equals(that.getId());
+        Categoria categoria = (Categoria) o;
+        return getId().equals(categoria.getId());
     }
 
     @Override
---
+++ src/main/java/com/amon/cursomc/domain/Produto.java
@@ -0,0 +1,78 @@
+package com.amon.cursomc.domain;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+public class Produto implements Serializable {
+    private static final long serialVersionUID = -2589139918912750956L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private double preco;
+
+    @ManyToMany
+    @JoinTable( name = "PRODUTO_CATEGORIA",
+        joinColumns = @JoinColumn(name = "produto_id"),
+        inverseJoinColumns = @JoinColumn(name = "categoria_id")
+    )
+    private List<Categoria> categorias = new ArrayList<>();
+
+    public Produto() {
+    }
+
+    public Produto(Integer id, String name, double preco) {
+        this.id = id;
+        this.name = name;
+        this.preco = preco;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public List<Categoria> getCategorias() {
+        return categorias;
+    }
+
+    public void setCategorias(List<Categoria> categorias) {
+        this.categorias = categorias;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Produto)) return false;
+        Produto produto = (Produto) o;
+        return getId().equals(produto.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}

4e168b251cc1d90ef17b004dd5462887e9fdb8d8
Amon Silva <Amon Silva@italtel.com.br>
2021-06-07 09:50:21 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/amon/cursomc/services/ClienteService.java
@@ -0,0 +1,22 @@
+package com.amon.cursomc.services;
+
+import com.amon.cursomc.domain.Cliente;
+import com.amon.cursomc.repositories.ClienteRepository;
+import com.amon.cursomc.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository repo;
+
+    public Cliente buscar(Integer id){
+        Optional<Cliente> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+    }
+}

9ec7d614b4809c59deef335152036fa6b112c6dc
Amon Silva <Amon Silva@italtel.com.br>
2021-06-14 08:46:22 -0300

    Personalizando o retorno da validacao

---
+++ src/main/java/com/amon/cursomc/resources/exception/FieldMessage.java
@@ -0,0 +1,33 @@
+package com.amon.cursomc.resources.exception;
+
+import java.io.Serializable;
+
+public class FieldMessage implements Serializable {
+    private static final long serialVersionUID = 428933437011600457L;
+    private String fieldName;
+    private String message;
+
+    public FieldMessage() {
+    }
+
+    public FieldMessage(String fieldName, String message) {
+        this.fieldName = fieldName;
+        this.message = message;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public void setFieldName(String fieldName) {
+        this.fieldName = fieldName;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}

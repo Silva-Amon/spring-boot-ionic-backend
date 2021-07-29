75e5ac48cbb4ed548c6fcf7d4f9d6f442a8bce22
Amon Silva <Amon Silva@italtel.com.br>
2021-07-29 17:31:13 -0300

    Salvando o primeiro arquivo no S3

--- src/main/java/com/amon/cursomc/CursomcApplication.java
+++ src/main/java/com/amon/cursomc/CursomcApplication.java
@@ -4,6 +4,7 @@ import com.amon.cursomc.domain.*;
 import com.amon.cursomc.domain.enums.EstadoPagamento;
 import com.amon.cursomc.domain.enums.TipoCliente;
 import com.amon.cursomc.repositories.*;
+import com.amon.cursomc.services.S3Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -15,13 +16,16 @@ import java.util.Arrays;
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
+	@Autowired
+	private S3Service s3Service;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		s3Service.uploadFile("carro.jpg");
 	}
 }
 
